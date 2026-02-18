4b9b0ed4a54f7afb106cc9cdba2eb7c65e37c606
Marcio Jr <jmbtj.mb.15@gmail.com>
2026-02-18 13:00:36 -0300

    feat: implement check-in list display and removal functionality

--- src/pages/ListPage/ListPage.tsx
+++ src/pages/ListPage/ListPage.tsx
@@ -1,5 +1,43 @@
+import styles from './styles.module.css';
+import { useCheckInStore } from '../../store';
+import { Trash2Icon } from 'lucide-react';
 import { MusicPlayer } from '../../components/MusicPlayer';
 
 export function ListPage() {
-  return <MusicPlayer />;
+  const checkIns = useCheckInStore(s => s.checkIns);
+  const removeCheckIn = useCheckInStore(s => s.removeCheckIn);
+
+  return (
+    <div className={styles.container}>
+      {checkIns.length > 0 && (
+        <div className={styles.responsiveTable}>
+          <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Flight</th>
+              </tr>
+            </thead>
+            <tbody>
+              {checkIns.map(({ name, flight }, index) => (
+                <tr key={`${name}-${flight}-${index}`}>
+                  <td>{name}</td>
+                  <td className={styles.flightCell}>
+                    <span>{flight}</span>
+                    <button
+                      className={styles.buttonTrash}
+                      onClick={() => removeCheckIn(index)}
+                    >
+                      <Trash2Icon size={18} />
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
+      <MusicPlayer />
+    </div>
+  );
 }
